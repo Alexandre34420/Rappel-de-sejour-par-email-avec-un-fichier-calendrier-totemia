dd3e844c35d75306232562422ba1a2c4a441c2b6
Alexandre Lafage-Landry <alexandre.lafage-landry@totemia.com>
2026-05-07 12:15:42 +0200

    suite docker

--- backend/index.js
+++ backend/index.js
@@ -63,6 +63,10 @@ app.post("/api/send", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("✅ Backend API is running");
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Backend running on port ${PORT}`);
 });
