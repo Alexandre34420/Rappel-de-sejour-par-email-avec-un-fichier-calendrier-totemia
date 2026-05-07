1a9bb1eba848fab50cb689ea4400d5f8a2d2d31d
Alexandre Lafage-Landry <alexandre.lafage-landry@totemia.com>
2026-05-07 10:27:50 +0200

    modifs docker

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,9 @@ app.post("/api/send", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Backend running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
 
